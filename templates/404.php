735a6ea61c75cfec92a9b06903486f18cff90ae4
Jalquin <jalquin@seznam.cz>
2023-03-07 18:59:21 +0100

    Version 3.1
    adding 404 page

---
+++ templates/404.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row text-center">
+        <h1>Stránka nenalezena!</h1>
+        <div class="col">
+            <h2>Zkuste se vrátit na <a href="index.php?pages=main">úvodní stránku</a>...</h2>
+            <img alt="404"
+                 class="img-fluid"
+                 src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg">
+        </div>
+    </div>
+</div>
