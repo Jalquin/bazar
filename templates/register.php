033f8d494a42385f4571f759cb181b0a08d270c0
Jan <honzikraj@gmail.com>
2023-04-01 14:54:47 +0200

    1.4. fixing functionalities

--- templates/register.php
+++ templates/register.php
@@ -1,3 +1,36 @@
+<?php
+$error = "";
+
+
+if (isset($_POST['submit'])) {
+
+    $name = htmlspecialchars($_POST["name"]);
+    $surname = htmlspecialchars($_POST["surname"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = md5(htmlspecialchars($_POST["password"]));
+
+    //echo $name . $surname . $email . $password;
+
+    //check if registered
+    $checkifregistered = mysqli_query($conn, "SELECT email FROM Uzivatel WHERE email = '$email'");
+    if (mysqli_num_rows($checkifregistered) > 0) {
+        $error = "An user is already registered with this email.<br>";
+
+    } else {
+        //ZDE MŮŽEME PŘÍPADNĚ ROZŠIŘOVAT DALŠÍ OŠETŘENÍ
+        $dotaz = "INSERT INTO Uzivatel (jmeno,prijmeni,email,heslo,opravneni,obrazky_id) VALUES (?,?,?,?,0,1)";
+        $stmt = $conn->prepare($dotaz);
+        $stmt->bind_param("ssss", $name, $surname, $email, $password);
+        $stmt->execute();
+        $id = $stmt->insert_id;
+        $error = "You have registered sucessfully!";
+    }
+}
+
+echo $error;
+
+?>
+
 <div class="container">
     <div class="row">
         <div class="col">
@@ -17,7 +50,7 @@
     <div class="row mb-2">
         <div class="col-12 col-lg-6">
             <h1>Registrace</h1>
-            <form action="scripts/register.php" method="POST">
+            <form action="" method="POST">
 
                 <div class="mb-3">
                     <label class="form-label" for="name"><i class="bi bi-person"></i> Jméno</label>
@@ -61,3 +94,4 @@
         </div>
     </div>
 </div>
+
